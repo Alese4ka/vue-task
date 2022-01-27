502012ba3567e237e825a85b01c5e9390e2c4730
Alese4ka <alesun4ik@gmail.com>
2022-01-27 11:11:03 +0300

    feature/TD-4: -change mistakes

--- src/components/organisms/TodoList.vue
+++ src/components/organisms/TodoList.vue
@@ -39,48 +39,54 @@ export default {
 
 <style lang="scss" module>
 @import "@/assets/scss/main.scss";
-body{
-  background: $wrapper;
-  .main{
-    margin: 0 auto;
-    max-width: 73.625rem;
-    .wrapper{
-      background: url("~@/assets/img/vector.svg");
-      background-repeat: no-repeat;
-      background-position-x: 40.625rem;
-      background-position-y: 4.375rem;
-      .todo{
-        max-width: 33.75rem;
-        margin: 0 auto;
-        padding-top: 6.1875rem;
-        padding-bottom: 6.625rem;
-        min-height: 32.5rem;
-        .todolist{
-          background-color: $todolist;
-          h1{
-            @include font-open ();
-            text-align: center;
-            color: $h1;
-            background-color: $h1back;
-            margin: 0;
-            padding: 0.6875rem 0;
-          }
-          .action{
-            padding: 0.5rem 0 0.5rem 1rem;
-            background-color:  $h1back;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            color: $action;
-            @include font-open ();
-            @media (max-width: 28.125rem) {
-              font-size: 0.8rem;
-              flex-direction: column;
+html{
+  min-height: 100vh;
+  body{
+    background: $wrapper;
+    margin: 0;
+    box-sizing: border-box;
+    .main{
+      margin: 0 auto;
+      margin-top: 60px;
+      max-width: 73.625rem;
+      .wrapper{
+        background: url("~@/assets/img/vector.svg");
+        background-repeat: no-repeat;
+        background-position-x: 40.625rem;
+        background-position-y: 4.375rem;
+        .todo{
+          max-width: 33.75rem;
+          margin: 0 auto;
+          padding-top: 6.1875rem;
+          padding-bottom: 6.625rem;
+          min-height: 32.5rem;
+          .todolist{
+            background-color: $todolist;
+            h1{
+              @include font-open ();
+              text-align: center;
+              color: $h1;
+              background-color: $h1back;
+              margin: 0;
+              padding: 0.6875rem 0;
             }
-          } 
+            .action{
+              padding: 0.5rem 0 0.5rem 1rem;
+              background-color:  $h1back;
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              color: $action;
+              @include font-open ();
+              @media (max-width: 28.125rem) {
+                font-size: 0.8rem;
+                flex-direction: column;
+              }
+            } 
+          }
         }
-      }
-    } 
+      } 
+    }
   }
 }
 </style>

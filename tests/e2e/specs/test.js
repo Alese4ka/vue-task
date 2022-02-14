c3a6eeab23ac416dbbe0a9a012f9d946d135fc20
Alesya Branavitskaya <alesun4ik@gmail.com>
2022-02-14 16:57:09 +0300

    feature/TD-7: -create e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,23 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
-  });
+import { text } from "body-parser";
+import { type } from "os";
+
+describe('My First Test', () => {
+  //beforeEach(() => {
+    it('test contents', () => {
+      cy.visit('http://localhost:8080/');
+
+      cy.contains('h1', 'to do list');
+      cy.contains('p', 'No tasks');
+      
+      cy.get('input[type=text]').type('text').type('{enter}')
+      cy.get('input[type=checkbox]').check({force: true}); 
+      cy.get('input[type=text]').type('text1').type('{enter}')
+      
+      cy.get('input[type="radio"]').check('1', { force: true }).should('be.checked')
+
+      cy.get('button').trigger('click');  
+    });
+  //})
 });

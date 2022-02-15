9435be67be43bbed73f032c4ce57732df8e1b498
Alesya Branavitskaya <alesun4ik@gmail.com>
2022-02-15 19:48:30 +0300

    feature/TD-7: -create tests for array and check label

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,19 +4,24 @@ import { text } from "body-parser";
 import { type } from "os";
 
 describe('My First Test', () => {
-  //beforeEach(() => {
     it('test contents', () => {
+      //Open the site
       cy.visit('http://localhost:8080/');
-
+      //Check texts in h1,p
       cy.contains('h1', 'to do list');
       cy.contains('p', 'No tasks');
-      
+      //Check addTask function 
       cy.get('input[type=text]').type('text').type('{enter}')
+      //Check checkbox
       cy.get('input[type=checkbox]').check({force: true}); 
       cy.get('input[type=text]').type('text1').type('{enter}')
-      
+      //Check label include 'text'
+      cy.contains('label', 'text');
+      //Check length array
+      cy.get('input[type=checkbox]').should('have.length', 2)
+      //Check tabs
       cy.get('input[type="radio"]').check('1', { force: true }).should('be.checked')
-
+      //Check delete function
       cy.get('button').trigger('click');  
     });
   //})
